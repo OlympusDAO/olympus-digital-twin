aa557d4375bebe60a1dce76fedf9c98d86a3846e
Sean McOwen <seanmcowen@gmail.com>
2022-12-20 11:26:56 -0500

    p_real_ask_capacity_cushion

--- model/policy/treasury_market_operations.py
+++ model/policy/treasury_market_operations.py
@@ -17,3 +17,22 @@ def real_bid_capacity_cushion_policy(bid_counter, min_counter_reinstate, with_re
         bid_capacity_cushion = bid_capacity_target_cushion
 
     return bid_capacity_cushion
+
+
+def real_ask_capacity_cushion_policy(ask_counter, min_counter_reinstate, with_reinstate_window, natural_price, upper_target_cushion, ask_capacity_target_cushion, upper_target_wall, ask_capacity_cushion_prior, net_flow, reserves_in, liq_stables_prior, amm_k):
+    if (sum(ask_counter) >= min_counter_reinstate or with_reinstate_window == 'No') and natural_price < upper_target_cushion:
+        ask_capacity_cushion = ask_capacity_target_cushion
+    elif natural_price > upper_target_cushion and natural_price <= upper_target_wall:
+        ask_capacity_cushion = upper_target_cushion and ask_capacity_cushion_prior - \
+            (net_flow - reserves_in + liq_stables_prior) / \
+            upper_target_cushion + \
+            (amm_k / upper_target_cushion) ** (1/2) or 0
+    else:
+        ask_capacity_cushion = ask_capacity_cushion_prior
+
+    if ask_capacity_cushion < 0:
+        ask_capacity_cushion = 0
+    elif ask_capacity_cushion > ask_capacity_target_cushion:
+        ask_capacity_cushion = ask_capacity_target_cushion
+
+    return ask_capacity_cushion
--- model/psub_functions/treasury_market_operations.py
+++ model/psub_functions/treasury_market_operations.py
@@ -1,4 +1,4 @@
-from ..policy.treasury_market_operations import real_bid_capacity_cushion_policy
+from ..policy.treasury_market_operations import real_bid_capacity_cushion_policy, real_ask_capacity_cushion_policy
 
 
 def p_real_bid_capacity_cushion(_params, substep, state_history, state) -> dict:
@@ -21,20 +21,22 @@ def p_real_bid_capacity_cushion(_params, substep, state_history, state) -> dict:
 
 
 def p_real_ask_capacity_cushion(_params, substep, state_history, state) -> dict:
-    """
-    if (sum(self.ask_counter) >= params.min_counter_reinstate or params.with_reinstate_window == 'No') and natural_price < self.upper_target_cushion:
-                self.ask_capacity_cushion = self.ask_capacity_target_cushion
-            elif natural_price > self.upper_target_cushion and natural_price <= self.upper_target_wall:
-                self.ask_capacity_cushion = self.upper_target_cushion and prev_day.ask_capacity_cushion - (self.net_flow - self.reserves_in + prev_day.liq_stables) / self.upper_target_cushion + (self.k / self.upper_target_cushion) ** (1/2) or 0
-            else:
-                self.ask_capacity_cushion = prev_day.ask_capacity_cushion
+    prev_day = state_history[-1][-1]
 
-            if self.ask_capacity_cushion < 0:
-                self.ask_capacity_cushion = 0
-            elif self.ask_capacity_cushion > self.ask_capacity_target_cushion:
-                self.ask_capacity_cushion = self.ask_capacity_target_cushion
-    """
-    return {"ask_capacity_cushion": state["ask_capacity_cushion"]}
+    ask_counter = state["ask_counter"]
+    min_counter_reinstate = _params["min_counter_reinstate"]
+    with_reinstate_window = _params["with_reinstate_window"]
+    natural_price = state["natural_price"]
+    upper_target_cushion = state["upper_target_cushion"]
+    ask_capacity_target_cushion = state["ask_capacity_target_cushion"]
+    upper_target_wall = state["upper_target_wall"]
+    ask_capacity_cushion_prior = prev_day["ask_capacity_cushion"]
+    net_flow = state["net_flow"]
+    reserves_in = state["reserves_in"]
+    liq_stables_prior = prev_day["liq_stables"]
+    amm_k = state["amm_k"]
+
+    return {"ask_capacity_cushion": real_ask_capacity_cushion_policy(ask_counter, min_counter_reinstate, with_reinstate_window, natural_price, upper_target_cushion, ask_capacity_target_cushion, upper_target_wall, ask_capacity_cushion_prior, net_flow, reserves_in, liq_stables_prior, amm_k)}
 
 
 def s_bid_capacity_cushion(_params, substep, state_history, state, _input) -> tuple:
