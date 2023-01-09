766a62e5e5110b9692d38df338834d05168e5268
Sean McOwen <seanmcowen@gmail.com>
2023-01-09 11:47:03 +0800

    Change the bond discount factor

--- model/behavioral/ohmbond.py
+++ model/behavioral/ohmbond.py
@@ -1,19 +1,16 @@
 import numpy as np
-def price_curve(expiration_duration:int,discount_factor = 1/5)->float:
-    # given the duration, return a discount rate (should be between (0,1) for rational decision-makers...)
-    # longer the duration is, bigger discount it should be
-    # discount factor shoudl be smaller than 1, bigger than 0. bigger discount factor, more temporal discount (meaning the longer expiration duration bonds will be cheaper)
-    # future to-do: 1) find better estimates or allow flexible change of the assumption
-    assert discount_factor < 1
-    discounted_price = 1/(expiration_duration/28)**discount_factor # 28 assuming for a 30 day bond the discount rate is close to 1
-    discounted_price = max(1,discounted_price + np.random.randn()*.01)
-    return discounted_price
 
-def bond_sell_batch_auction(bonds:list):
+
+def price_curve(expiration_duration: int, discount_rate: float) -> float:
+    # Returns the discount factor to be applied to the bond
+    discount_factor = 1 / (1 + discount_rate) ** (expiration_duration/365)
+    return discount_factor
+
+
+def bond_sell_batch_auction(bonds: list, discount_rate: float):
     total_sell_amount = 0
     for bond in bonds:
-        av_price = price_curve(bond.expiration_duration)
+        av_price = price_curve(bond.expiration_duration, discount_rate)
         sell_amount = bond.total_amount*av_price
         total_sell_amount += sell_amount
     return total_sell_amount
-    
