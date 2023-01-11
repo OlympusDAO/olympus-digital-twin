9ba958ca2637d79d59a0aee08bbdfc731e2d9619
zhiwei <0lizhiwei0@gmail.com>
2023-01-10 22:16:51 -0700

    added additional RBS analysis with parameter sweeping and price tracking

--- model/utility/visualization.py
+++ model/utility/visualization.py
@@ -1,6 +1,7 @@
 from typing import List
 from pandas import DataFrame
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_all_sims(var_list: List[str], df: DataFrame):
@@ -17,3 +18,22 @@ def plot_grouped_variables_average(var_list: List[str], grouping_variables: List
         data[col].unstack(-1).T.plot(kind='line')
         plt.title(col)
         plt.show()
+    return plt.figure
+
+def plot_multivars_grouped_average(var_list: list[str], grouping_variables: list[str], df: DataFrame,linestyles:list=[]):
+    grpvar_vals = np.unique(df[grouping_variables],axis=0)
+    data = df.groupby(grouping_variables + ["timestep"])[var_list].mean()#.reset_index(grouping_variables)
+    fig,axes = plt.subplots(nrows=len(grpvar_vals),figsize=(4*len(grpvar_vals),20))
+
+    if len(linestyles)==0:
+        linestyles = ['-'] * len(var_list)
+    for k,ax in enumerate(axes):
+        for plotvar,lstyle in zip(var_list,linestyles):
+            ax.plot(data[plotvar].unstack(-1).T[tuple(grpvar_vals[k])],label=plotvar,linestyle = lstyle)
+            ax.legend()
+        titlestr = ''
+        for var,varval in zip(grouping_variables,grpvar_vals[k]):
+            titlestr += f'{var} = {varval}, '
+        ax.set_title(titlestr[:-2])
+    fig.show()
+    return fig
