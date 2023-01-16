0bf0f182fb451e6b05e9bd927aab8aa90495cc23
Sean McOwen <seanmcowen@gmail.com>
2023-01-16 13:27:51 -0500

    Finish period bond creation

--- model/signals/bond_signals.py
+++ model/signals/bond_signals.py
@@ -1,7 +1,14 @@
 from pandas import DataFrame
-from model.types.primitives import day
+from model.types.primitives import day, OHM
+from typing import List
+from model.policy.ohmbond import generate_ohmbond
 
 
-def periodic_bond_creation(period: day) -> DataFrame:
+def periodic_bond_creation(period: day, bond_tenors: List[day], face_value: OHM, simulation_timesteps: day) -> DataFrame:
+    start_times = list(range(1, simulation_timesteps, period))
+    face_value_temp = [[face_value] * len(bond_tenors)] * len(start_times)
+    bond_tenors_temp = [bond_tenors] * len(start_times)
+    bonds = generate_ohmbond(amounts=face_value_temp,
+                             exp_durs=bond_tenors_temp, start_days=start_times)
 
-    return None
+    return bonds
