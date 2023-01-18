43b86b7a8d7ea004487b2ffad07f054bc2c69eb6
Sean McOwen <seanmcowen@gmail.com>
2023-01-18 10:23:39 -0500

    Add documentation to create_par_sweep

--- model/utility/par_sweep.py
+++ model/utility/par_sweep.py
@@ -1,7 +1,27 @@
 from itertools import product
+from copy import deepcopy
 
-def create_par_sweep(sweep_dict:dict):
+
+def create_par_sweep(sweep_dict: dict) -> dict:
+    """This function takes a dictionary where key is parameter name
+    and value is a list of all possible values for that parameter. It
+    converts this into a dictionary with the same keys but with values
+    that are the cartesian product of all the lists.
+
+    Args:
+        sweep_dict (dict): A dictionary of params and values to sweep over
+
+    Returns:
+        dict: A dictionary of cartesian product params
+    """
+    # Copy sweep_dict to avoid overwriting
+    sweep_dict = deepcopy(sweep_dict)
+
+    # Get the cartesian product
     sweeps = list(product(*sweep_dict.values()))
+
+    # Assign values
     for kk, varname in enumerate(sweep_dict.keys()):
-        sweep_dict[varname]=[x[kk] for x in sweeps]
+        sweep_dict[varname] = [x[kk] for x in sweeps]
+
     return sweep_dict
