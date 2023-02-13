ec07f0903a67cb707938b1e4ce0f0a9558d8714e
zhiwei <0lizhiwei0@gmail.com>
2023-02-13 15:58:04 -0700

    allowing the colormap to change so when there's too many lines it'll be easier to see (this change may change the previous figures' color a bit)

--- model/utility/visualization.py
+++ model/utility/visualization.py
@@ -55,16 +55,16 @@ def plot_all_sims(var_list: List[str], df: DataFrame):
         plt.legend([])
         plt.show()
 
-
-def plot_grouped_variables_average(var_list: List[str], grouping_variables: List[str], df: DataFrame,legend_loc=''):
+def plot_grouped_variables_average(var_list: List[str], grouping_variables: List[str], df: DataFrame,legend_loc='',colormap = 'tab10'):
     data = df.groupby(grouping_variables + ["timestep"])[var_list].mean()
     for col in var_list:
-        data[col].unstack(-1).T.plot(kind='line')
+        data[col].unstack(-1).T.plot(kind='line',colormap=colormap)
         plt.title(col)
         if len(legend_loc)>0:
             plt.legend(loc=legend_loc)
         plt.show()
     return plt.figure
+
 
 
 def plot_multivars_grouped_average(var_list: list[str], grouping_variables: list[str], df: DataFrame, linestyles: list = []):
